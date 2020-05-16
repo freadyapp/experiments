6c9b9e01835484e092b28b8ad79a44924253e0ed
robo-monk <filtap007@gmail.com>
2020-05-17 02:26:29 +0300

    difficult_words

---
+++ hard_words/make_the_shitty_list_a_good_list.py
@@ -0,0 +1,21 @@
+import csv
+import json
+
+plip = {}
+
+def allocate_word(w):
+  key = w[0].lower() + str(len(w))
+  
+  if not key in plip:
+    plip[key] = []
+    
+  plip[key].append(w)
+
+
+with open('difficult_words_shitty_list.csv', 'r') as csvfile:
+  shity_list = csv.reader(csvfile, delimiter=' ', quotechar='|')
+  for row in shity_list:
+    allocate_word(row[0].split(',')[0])
+    
+  with open("good_list.txt", "w") as text_file:
+    text_file.write(json.dumps(plip))
